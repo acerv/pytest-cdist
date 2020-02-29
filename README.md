ea8bf71f6ade0469ccc341672d26c1a8e39da280
Andrea Cervesato <andrea.cervesato@mailbox.org>
2020-02-29 15:31:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+TODO
+====
+
+[ ] client class to read/write pytest configurations
+[ ] cdist-cli command
+[ ] pytest-cdist plugin
+[ ] redis support
+[ ] redis client username/password support
+[ ] redis client ssl keyfile support
---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+.. module:: setup
+    :platform: Multiplatform
+    :synopsis: installer module
+.. moduleauthor:: Andrea Cervesato <andrea.cervesato@mailbox.org>
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='pytest-cdist',
+    version='1.0',
+    description='Pytest plugin for distributed pytest configurations',
+    author='Andrea Cervesato',
+    author_email='andrea.cervesato@mailbox.org',
+    url='https://github.com/acerv/pytest-cdist',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    python_version=">3.4,<3.9",
+    install_requires=[
+        'click<=7.0',
+        'colorama<=0.4.3',
+        'redis<=3.4.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cdist-cli=cdist.command:client',
+        ],
+    },
+)
